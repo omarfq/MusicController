b5f1787ba618a595f46c5fc8c77ad1d3c6ab36b9
Omar Flores <omarflores@Omars-MacBook-Pro.local>
2021-07-24 19:16:22 -0400

    Added basic routes in frontend

---
+++ frontend/src/components/CreateRoomPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+
+export default class CreateRoomPage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <p>This is the create room pahe</p>;
+    }
+}
