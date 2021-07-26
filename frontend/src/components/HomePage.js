90ae0ac6ddc773c024a963caa6a05aee279f75cf
Omar Flores <omarflores@Omars-MacBook-Pro.local>
2021-07-26 06:25:15 -0400

    Added some methods to control the button on the Create/Update Room pages, added patch endpoint

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -18,6 +18,7 @@ export default class HomePage extends Component {
     this.state = {
       roomCode: null,
     };
+    this.clearRoomCode = this.clearRoomCode.bind(this);
   }
 
   async componentDidMount() {
@@ -52,6 +53,12 @@ export default class HomePage extends Component {
     );
   }
 
+  clearRoomCode() {
+    this.setState({
+      roomCode: null,
+    });
+  }
+
   render() {
     return (
       <Router>
@@ -63,13 +70,18 @@ export default class HomePage extends Component {
               return this.state.roomCode ? (
                 <Redirect to={`/room/${this.state.roomCode}`} />
               ) : (
-                this.renderHomePage
+                this.renderHomePage()
               );
             }}
           ></Route>
           <Route path="/join" component={RoomJoinPage} />
           <Route path="/create" component={CreateRoomPage} />
-          <Route path="/room/:roomCode" component={Room} />
+          <Route
+            path="/room/:roomCode"
+            render={(props) => {
+              return <Room {...props} leaveRoomCallback={this.clearRoomCode} />;
+            }}
+          />
         </Switch>
       </Router>
     );
